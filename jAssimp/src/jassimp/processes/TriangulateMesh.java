b7203aa57ff4d83279a63e386328ccaeed01ee1f
Giuseppe Barbieri <elect86@gmail.com>
2015-09-17 17:16:35 +0200

    Routine

--- jAssimp/src/jassimp/processes/TriangulateMesh.java
+++ jAssimp/src/jassimp/processes/TriangulateMesh.java
@@ -8,8 +8,11 @@ package jassimp.processes;
 import jassimp.components.AiFace;
 import jassimp.components.AiMesh;
 import static jassimp.components.AiPrimitiveType.aiPrimitiveType_POLYGON;
+import static jassimp.components.AiPrimitiveType.aiPrimitiveType_TRIANGLE;
 import jassimp.components.AiScene;
 import static jassimp.importing.AiPostProcessSteps.aiProcess_Triangulate;
+import java.util.ArrayList;
+import jglm.Vec3;
 
 /**
  *
@@ -64,19 +67,42 @@ public class TriangulateMesh extends BaseProcess {
         int numOut = 0, max_out = 0;
         boolean get_normals = true;
         for( int a = 0; a < pMesh.mNumFaces; a++) {
-//            AiFace face = pMesh->mFaces[a];
-//            if (face.mNumIndices <= 4) {
-//                get_normals = false;
-//            }
-//            if( face.mNumIndices <= 3) {
-//                numOut++;
-//
-//            }
-//            else {
-//                numOut += face.mNumIndices-2;
-//                max_out = std::max(max_out,face.mNumIndices);
-//            }
+            AiFace face = pMesh.mFaces[a];
+            if (face.mNumIndices <= 4) {
+                get_normals = false;
+            }
+            if( face.mNumIndices <= 3) {
+                numOut++;
+            }
+            else {
+                numOut += face.mNumIndices-2;
+                max_out = Math.max(max_out,face.mNumIndices);
+            }
         }
+        
+        // Just another check whether aiMesh::mPrimitiveTypes is correct
+        if(numOut == pMesh.mNumFaces){
+            throw new Error("numOut == pMesh.mNumFaces");
+        }
+        
+        Vec3 nor_out = null;
+        
+        // if we don't have normals yet, but expect them to be a cheap side
+        // product of triangulation anyway, allocate storage for them.
+        if (pMesh.mNormals == null && get_normals) {
+            // XXX need a mechanism to inform the GenVertexNormals process to treat these normals as preprocessed per-face normals
+        //  nor_out = pMesh->mNormals = new aiVector3D[pMesh->mNumVertices];
+        }
+    
+        // the output mesh will contain triangles, but no polys anymore
+        pMesh.mPrimitiveTypes |= aiPrimitiveType_TRIANGLE.value;
+        pMesh.mPrimitiveTypes &= ~aiPrimitiveType_POLYGON.value;
+        
+        AiFace[] out = new AiFace[numOut];
+        AiFace[] curOut = out;
+        ArrayList<Vec3> temp_verts3d (max_out+2); /* temporary storage for vertices */
+        std::vector<aiVector2D> temp_verts(max_out+2);
+        
         return false;
     }
 }
