c507f2ff6695249169b31207a9ed9f848ff3abfe
elect <elect86@gmail.com>
2017-01-29 18:55:33 +0100

    - refreshed models

--- src/test/resources/models/glTF/BoxTextured-glTF/BoxTextured0FS.glsl
+++ src/test/resources/models/glTF/BoxTextured-glTF/BoxTextured0FS.glsl
@@ -4,7 +4,7 @@ varying vec2 v_texcoord0;
 uniform sampler2D u_diffuse;
 uniform vec4 u_specular;
 uniform float u_shininess;
-void main.main(void) {
+void main(void) {
 vec3 normal = normalize(v_normal);
 vec4 color = vec4(0., 0., 0., 0.);
 vec4 diffuse = vec4(0., 0., 0., 1.);
--- src/test/resources/models/glTF/CesiumMilkTruck/CesiumMilkTruck1VS.glsl
+++ src/test/resources/models/glTF/CesiumMilkTruck/CesiumMilkTruck1VS.glsl
@@ -7,7 +7,7 @@ uniform mat4 u_modelViewMatrix;
 uniform mat4 u_projectionMatrix;
 attribute vec2 a_texcoord0;
 varying vec2 v_texcoord0;
-void main.main(void) {
+void main(void) {
 vec4 pos = u_modelViewMatrix * vec4(a_position,1.0);
 v_normal = u_normalMatrix * a_normal;
 v_texcoord0 = a_texcoord0;
