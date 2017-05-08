4aa42320a2c492cfa453e095f20360137926acf9
elect86 <elect86@gmail.com>
2017-05-08 23:41:53 +0200

    up

--- src/main/kotlin/assimp/Importer.kt
+++ src/main/kotlin/assimp/Importer.kt
@@ -76,7 +76,7 @@ class Importer(
         //TODO if (pimpl.mScene != null) FreeScene()
         Log.d("Assimp", "in")
         // First check if the file is accessible at all
-        Log.d("Assimp", "${assets.open(path)}")
+        Log.d("Assimp", if(assets.open(path) == null) "null" else "!null")
         val stream = assets.open(path) ?: throw Error("Unable to open stream: $path")
         Log.d("Assimp", "in2")
         // Find an worker class which can handle the file
