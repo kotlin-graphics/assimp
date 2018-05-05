2e2de2784630c4d68d2567eecf1b0847112c4151
elect86 <elect86@gmail.com>
2018-05-05 22:46:28 +0200

    restored collada test coverage

--- src/test/kotlin/assimp/collada/collada.kt
+++ src/test/kotlin/assimp/collada/collada.kt
@@ -9,9 +9,9 @@ class collada  : StringSpec() {
     val path = "$models/Collada/"
 
     init {
-//        "anims full rot"  { animFullRot(getResource("$path/anims_with_full_rotations_between_keys.DAE")) }
-//        "cameras"  { cameras(getResource("$path/cameras.dae")) }
-//        "concave poly"  { concavePoly(getResource("$path/ConcavePolygon.dae")) }
+        "anims full rot"  { animFullRot(getResource("$path/anims_with_full_rotations_between_keys.DAE")) }
+        "cameras"  { cameras(getResource("$path/cameras.dae")) }
+        "concave poly"  { concavePoly(getResource("$path/ConcavePolygon.dae")) }
         "treasure smooth"  { treasure_smooth(getResource("$path/treasure_smooth.dae")) }
     }
 }
