0c7d00d227e8fd2fdb04bc6916dbee60df051026
elect86 <elect86@gmail.com>
2018-05-06 14:45:49 +0200

    restored test coverage

--- src/test/kotlin/assimp/collada/collada.kt
+++ src/test/kotlin/assimp/collada/collada.kt
@@ -9,11 +9,11 @@ class collada  : StringSpec() {
     val path = "$models/Collada/"
 
     init {
-//        "anims full rot"  { animFullRot(getResource("$path/anims_with_full_rotations_between_keys.DAE")) }
-//        "cameras"  { cameras(getResource("$path/cameras.dae")) }
-//        "concave poly"  { concavePoly(getResource("$path/ConcavePolygon.dae")) }
-//        "treasure smooth"  { treasure_smooth(getResource("$path/treasure_smooth.dae")) }
-//        "treasure smooth Pretransform"  { `treasure_smooth Pretransform`(getResource("$path/treasure_smooth.dae")) }
+        "anims full rot"  { animFullRot(getResource("$path/anims_with_full_rotations_between_keys.DAE")) }
+        "cameras"  { cameras(getResource("$path/cameras.dae")) }
+        "concave poly"  { concavePoly(getResource("$path/ConcavePolygon.dae")) }
+        "treasure smooth"  { treasure_smooth(getResource("$path/treasure_smooth.dae")) }
+        "treasure smooth Pretransform"  { `treasure_smooth Pretransform`(getResource("$path/treasure_smooth.dae")) }
         "color teapot spheres"  { `color teapot spheres`(getResource("$path/color_teapot_spheres.dae")) }
     }
 }
