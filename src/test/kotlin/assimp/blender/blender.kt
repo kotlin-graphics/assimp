77a0f84a72e70fb03d294f58923af8fafe699475
Burkhard Mittelbach <wasabi37a@googlemail.com>
2018-12-19 13:18:43 +0100

    enabled "blender default 250 compressed"
    Signed-off-by: Burkhard Mittelbach <wasabi37a@googlemail.com>

--- src/test/kotlin/assimp/blender/blender.kt
+++ src/test/kotlin/assimp/blender/blender.kt
@@ -13,7 +13,7 @@ class blender : StringSpec() {
     init {
 
         "testCubeRotateScaleTranslate" { testCubeRotateScaleTranslate(path + "testCubeRotateScaleTranslate.blend") }
-//        "blender default 250 compressed" { blenderDefault_250_compressed(path + "BlenderDefault_250_Compressed.blend") }
+        "blender default 250 compressed" { blenderDefault_250_compressed(path + "BlenderDefault_250_Compressed.blend") }
         // "spider"{ spider(path + "spider.obj") }
         // "nanosuit" { nanosuit(path + "nanosuit/nanosuit.obj") }
         // "shelter" { shelter(path + "statie B01.obj")}
