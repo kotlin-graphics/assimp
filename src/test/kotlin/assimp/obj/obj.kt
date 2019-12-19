db9528ea7129b98ad0c4744d9632f9ccaadbfaea
gbarbieri <elect86@gmail.com>
2019-12-19 13:07:41 +0100

    try build

--- src/test/kotlin/assimp/obj/obj.kt
+++ src/test/kotlin/assimp/obj/obj.kt
@@ -8,12 +8,12 @@ class obj : StringSpec() {
     val path = "$models/OBJ"
 
     init {
-//        "box"{ box("$path/box.obj") }
-//        "cube"{ cube("$path/cube.obj") }
-//        "nanosuit" { nanosuit("$path/nanosuit") }
+        "box"{ box("$path/box.obj") }
+        "cube"{ cube("$path/cube.obj") }
+        "nanosuit" { nanosuit("$path/nanosuit") }
         "wall"{ wall("$path/wall.obj") }
-//        "spider"{ spider.obj("$path/spider") }
-//        "shelter" { shelter("$path/shelter") }
-//        "car" { car("$path/car") }
+        "spider"{ spider.obj("$path/spider") }
+        "shelter" { shelter("$path/shelter") }
+        "car" { car("$path/car") }
     }
 }
